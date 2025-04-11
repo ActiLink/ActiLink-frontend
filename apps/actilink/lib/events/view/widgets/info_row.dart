d8328c04a096e520477beb5c2006cddffb0df487
Ula Obirek <93942367+uobirek@users.noreply.github.com>
2025-04-11 13:19:36 +0200

    Events screen & event details screen (#10)
    Added event and event details screens
    ---------
    
    Co-authored-by: Ula <Ula@DESKTOP-PL5UG2H>

---
+++ apps/actilink/lib/events/view/widgets/info_row.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:ui/ui.dart';
+
+class InfoRow extends StatelessWidget {
+  const InfoRow({
+    required this.icon,
+    required this.label,
+    required this.value,
+    super.key,
+  });
+  final IconData icon;
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Icon(icon, size: 20, color: AppColors.brand),
+          const SizedBox(width: 12),
+          Text(
+            '$label:',
+            style:
+                AppTextStyles.bodyMedium.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              value,
+              style: AppTextStyles.bodyMedium
+                  .copyWith(color: AppColors.textSecondary),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ apps/actilink/lib/models/event.dart
@@ -0,0 +1,48 @@
+class Event {
+  Event({
+    required this.eventId,
+    required this.title,
+    required this.organizerId,
+    required this.startTime,
+    required this.endTime,
+    required this.price,
+    required this.maxUsers,
+    required this.minUsers,
+    required this.description,
+    required this.user,
+    required this.relatedHobbies,
+    this.venue,
+    this.location,
+  });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      eventId: json['eventId'] as int,
+      organizerId: json['organizerId'] as int,
+      startTime: DateTime.parse(json['startTime'] as String),
+      endTime: DateTime.parse(json['endTime'] as String),
+      venue: json['venue'] as String?,
+      location: json['location'] as String?,
+      price: (json['price'] as num).toDouble(),
+      maxUsers: json['maxUsers'] as int,
+      minUsers: json['minUsers'] as int,
+      description: json['description'] as String?,
+      user: json['user'] as String?,
+      title: json['title'] as String?,
+      relatedHobbies: json['relatedHobbies'] as List<String>,
+    );
+  }
+  final int eventId;
+  final int organizerId;
+  final DateTime startTime;
+  final DateTime endTime;
+  final String? venue;
+  final String? location;
+  final double price;
+  final int maxUsers;
+  final int minUsers;
+  final String? description;
+  final String? user;
+  final String? title;
+  final List<String> relatedHobbies;
+}
