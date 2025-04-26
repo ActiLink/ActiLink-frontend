0ac29fa7e97a15b710529c247ce99ec06ddf38db
F1r3d3v <ogieniewski.dev@gmail.com>
2025-04-26 19:17:46 +0200

    Add business client repository

--- packages/core/lib/src/repositories.dart
+++ packages/core/lib/src/repositories.dart
@@ -1,4 +1,5 @@
 export 'repositories/auth_token_repository.dart';
+export 'repositories/business_client_repository.dart';
 export 'repositories/event_repository.dart';
 export 'repositories/hobby_repository.dart';
 export 'repositories/user_repository.dart';
