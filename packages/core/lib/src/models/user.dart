32a805a314d390c41525b19083c51a8dfcef1869
Wojtekl770 <160852305+Wojtekl770@users.noreply.github.com>
2025-05-01 22:37:21 +0200

    feature/hobby_edition (#17)
    * stronka do edycji hobby
    
    * poprawka w formatowaniu
    
    * Update packages/core/lib/src/models/user.dart
    
    Co-authored-by: Adam Ogieniewski <ogieniewski.dev@gmail.com>
    
    * Update packages/core/lib/src/models/user.dart
    
    Co-authored-by: Adam Ogieniewski <ogieniewski.dev@gmail.com>
    
    ---------
    
    Co-authored-by: Adam Ogieniewski <ogieniewski.dev@gmail.com>

--- packages/core/lib/src/models/user.dart
+++ packages/core/lib/src/models/user.dart
@@ -1,10 +1,12 @@
 import 'package:core/src/models/base_user.dart';
+import 'package:core/src/models/hobby.dart';
 
 class User extends BaseUser {
   const User({
     required super.id,
     required super.name,
     required super.email,
+    this.hobbies,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
@@ -12,18 +14,38 @@ class User extends BaseUser {
       id: json['id'] as String? ?? '',
       name: json['name'] as String? ?? '',
       email: json['email'] as String? ?? '',
+      hobbies: (json['hobbies'] as List<dynamic>?)
+          ?.map((e) => Hobby.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
   }
+
+  final List<Hobby>? hobbies;
 
   User copyWith({
     String? id,
     String? name,
     String? email,
+    List<Hobby>? hobbies,
   }) {
     return User(
       id: id ?? this.id,
       name: name ?? this.name,
       email: email ?? this.email,
+      hobbies: hobbies ?? this.hobbies,
     );
   }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'hobbies': hobbies?.map((h) => h.toJson()).toList(),
+    };
+  }
+
+  @override
+  List<Object?> get props => [...super.props, hobbies];
 }
