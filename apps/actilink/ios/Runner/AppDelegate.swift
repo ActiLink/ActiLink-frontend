69f2ad28afb27b5ba4cfab31f589413ee4c34b88
Wojciech Łapan <Wojtekl770@interia.pl>
2025-05-30 16:49:02 +0200

    mała poprawka by mnie adam nie pobił

--- apps/actilink/ios/Runner/AppDelegate.swift
+++ apps/actilink/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyB9P4S5LvluHOSPBeGShVOK_ZH5howcMqc")
+    GMSServices.provideAPIKey("${GOOGLE_MAPS_API}")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
